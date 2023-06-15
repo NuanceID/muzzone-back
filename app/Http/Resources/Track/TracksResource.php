5e9a36d7521107d6b36e05334f0022aa44beb204
levinpwnz_ <d.denchik@id.world>
2023-06-15 19:59:51 +0800

    fix for

--- app/Http/Resources/Track/TracksResource.php
+++ app/Http/Resources/Track/TracksResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Track;
 
+use App\Http\Resources\Artist\IndexArtistResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TracksResource extends JsonResource
@@ -13,7 +14,12 @@ class TracksResource extends JsonResource
             'name' => $this->resource->name,
             'description' => $this->resource->description,
             'cover' => $this->resource->getSingleImageUrl(),
-            'file' => $this->resource->getSingleMediaUrl()
+            'file' => $this->resource->getSingleMediaUrl(),
+            'album' => [
+                'id' => $this->resource->album->id,
+                'name' => $this->resource->album->name
+            ],
+            'artists' => IndexArtistResource::collection($this->resource->artists)
         ];
     }
 }
