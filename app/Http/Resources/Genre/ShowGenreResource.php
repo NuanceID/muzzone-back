dde41ca26fa9ef06bc9a44a87b629ba9998ccd29
levinpwnz_ <d.denchik@id.world>
2023-06-15 17:18:18 +0800

    fix for

--- app/Http/Resources/Genre/ShowGenreResource.php
+++ app/Http/Resources/Genre/ShowGenreResource.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\Genre;
 
+use App\Http\Resources\Track\ShowTrackResource;
+
 class ShowGenreResource extends BaseGenreResource
 {
+    public function toArray($request): array
+    {
+        $data = parent::toArray($request);
 
+        $data['tracks'] = ShowTrackResource::collection($this->resource->tracks);
+
+        return $data;
+    }
 }
