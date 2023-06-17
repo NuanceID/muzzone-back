3aaabe82e8072168b97cbc4cdcd427e8061c6095
levinpwnz_ <d.denchik@id.world>
2023-06-17 15:24:34 +0800

    fix for

--- app/Http/Resources/Category/ShowCategoryResource.php
+++ app/Http/Resources/Category/ShowCategoryResource.php
@@ -3,7 +3,14 @@
 namespace App\Http\Resources\Category;
 
 
+use App\Http\Resources\Track\TracksResource;
+
 class ShowCategoryResource extends BaseCategoryResource
 {
+    public function toArray($request): array
+    {
+        $data = parent::toArray($request);
 
+        $data['tracks'] = TracksResource::collection($this->resource->tracks);
+    }
 }
